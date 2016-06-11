4943a16a1eea70a46094c326e986b83c904440a0
ShuhratBek <shuhratbek.26@gmail.com>
2016-06-11 23:02:17 +0300

    removing some files

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/es6-shim/index.d.ts" />
-/// <reference path="globals/hammerjs/index.d.ts" />
-/// <reference path="globals/jasmine/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
