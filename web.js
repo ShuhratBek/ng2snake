115ca4a8b62cc1147ca68c0d2e3c11ebfd394bdc
ShuhratBek <shuhratbek.26@gmail.com>
2016-06-13 22:26:37 +0300

    update heroku settings

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // Serve static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/dist'));
 
 // Serve your app
 console.log('Served: http://localhost:' + port);
